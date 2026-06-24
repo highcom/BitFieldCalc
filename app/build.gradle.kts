c74ac954ef225cec08a63b60ed451aa07087c091
koichi highcom <youhappywith@yahoo.co.jp>
2026-06-24 09:53:48 +0900

    広告ユニットIDの修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.highcom.bitfieldcalc"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
