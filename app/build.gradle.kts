6199c3992d32809b8e3c84966e5156395a06da4b
koichi highcom <youhappywith@yahoo.co.jp>
2026-07-08 08:40:52 +0900

    version 1.2.0の署名付きApp BundleとAPKファイルの作成

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.highcom.bitfieldcalc"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
